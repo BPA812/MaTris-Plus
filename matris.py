57f45d9413e6c14029bff2b6e31f74d43f34d610
“bpa812” <braxton.arentz@outlook.com>
2024-05-10 16:12:52 +1000

    extra shortening

--- matris.py
+++ matris.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import pygame
-from pygame import Rect, Surface
+from pygame import Rect, Surface 
 import random as r
 import os
 import kezmenu
@@ -16,7 +16,7 @@ WIDTH = MATRIX_WIDTH*BS + BW*2 + MATRIS_OFFSET*2 + LEFT_MARGIN
 HEIGHT,TRICKY_CENTERX,VISIBLE_MH = (MH-2)*BS + BW*2 + MATRIS_OFFSET*2,WIDTH-(WIDTH-(MATRIS_OFFSET+BS*MATRIX_WIDTH+BW*2))/2, MH - 2
 class Matris(object):
     def __init__(self):
-        self.surface = screen.subsurface(Rect((MATRIS_OFFSET+BW, MATRIS_OFFSET+BW),
+        self.sf = screen.subsurface(Rect((MATRIS_OFFSET+BW, MATRIS_OFFSET+BW),
                                               (MATRIX_WIDTH * BS, (MH-2) * BS)))
         self.matrix = dict()
         for y in range(MH):
@@ -28,35 +28,35 @@ class Matris(object):
         self.base_d_s = 0.4
         self.mk,self.mk_s = {'left': 0, 'right': 0},0.05
         self.mk_t = (-self.mk_s)*2
-        self.level,self.score,self.lines,self.combo = 1,0,0,1
-        self.hs,self.played_hsbeaten_sound,self.paused = load_score(),False,False
-        self.levelup_sound,self.gameover_sound,self.linescleared_sound,self.hsbeaten_sound = get_sound("levelup.wav"),get_sound("gameover.wav"),get_sound("linecleared.wav"),get_sound("highscorebeaten.wav")
+        self.lvl,self.sce,self.ls,self.combo = 1,0,0,1
+        self.hs,self.played_hbs,self.p = load_score(),False,False
+        self.lus,self.gos,self.lcs,self.hbs = get_sound("lvlup.wav"),get_sound("gameover.wav"),get_sound("lcleared.wav"),get_sound("highscorebeaten.wav")
     def set_tmes(self):
         self.current_tm,self.next_tm = self.next_tm,r.choice(list_of_tetrominoes)
-        self.surface_of_next_tm = self.construct_surface_of_next_tm()
-        self.tm_p,self.tm_r,self.tm_block,self.shadow_block = (0,4) if len(self.current_tm.shape) == 2 else (0, 3),0,self.block(self.current_tm.color),self.block(self.current_tm.color, shadow=True)
+        self.sf_of_next_tm = self.construct_sf_of_next_tm()
+        self.tm_p,self.tm_r,self.tm_block,self.shadow_block = (0,4) if len(self.current_tm.sh) == 2 else (0, 3),0,self.block(self.current_tm.color),self.block(self.current_tm.color, shadow=True)
     def hard_drop(self):
         amount = 0
         while self.request_movement('down'):
             amount += 1
-        self.score += 10*amount
+        self.sce += 10*amount
         self.lock_tm()
     def update(self, timepassed):
-        self.needs_redraw = False
+        self.n_r = False
         pr = lambda key: event.type == pygame.KEYDOWN and event.key == key
         unpr = lambda key: event.type == pygame.KEYUP and event.key == key
         events = pygame.event.get()
         for event in events:
             if pr(pygame.K_p):
-                self.surface.fill((0,0,0))
-                self.needs_redraw = True
-                self.paused = not self.paused
+                self.sf.fill((0,0,0))
+                self.n_r = True 
+                self.p = not self.p
             elif event.type == pygame.QUIT:
                 self.gameover(full_exit=True)
             elif pr(pygame.K_ESCAPE):
                 self.gameover()
-        if self.paused:
-            return self.needs_redraw
+        if self.p:
+            return self.n_r
         for event in events:
             if pr(pygame.K_SPACE):
                 self.hard_drop()
@@ -74,7 +74,7 @@ class Matris(object):
             elif unpr(pygame.K_RIGHT) or unpr(pygame.K_d):
                 self.mk['right'] = 0
                 self.mk_t = (-self.mk_s)*2
-        self.d_s = self.base_d_s ** (1 + self.level/10.)
+        self.d_s = self.base_d_s ** (1 + self.lvl/10.)
         self.d_ += timepassed
         d_s = self.d_s*0.10 if any([pygame.key.get_pressed()[pygame.K_DOWN],pygame.key.get_pressed()[pygame.K_s]]) else self.d_s
         if self.d_ > d_s:
@@ -86,20 +86,20 @@ class Matris(object):
         if self.mk_t > self.mk_s:
             self.request_movement('right' if self.mk['right'] else 'left')
             self.mk_t %= self.mk_s
-        return self.needs_redraw
-    def draw_surface(self):
+        return self.n_r
+    def draw_sf(self):
         with_tm = self.blend(matrix=self.place_shadow())
         for y in range(MH):
             for x in range(MATRIX_WIDTH):
-                block_location = Rect(x*BS, (y*BS - 2*BS), BS, BS)
+                bl = Rect(x*BS, (y*BS - 2*BS), BS, BS)
                 if with_tm[(y,x)] is None:
-                    self.surface.fill(BC, block_location)
+                    self.sf.fill(BC, bl)
                 else:
                     if with_tm[(y,x)][0] == 'shadow':
-                        self.surface.fill(BC, block_location)
-                    self.surface.blit(with_tm[(y,x)][1], block_location)
+                        self.sf.fill(BC, bl)
+                    self.sf.blit(with_tm[(y,x)][1], bl)
     def gameover(self, full_exit=False):
-        write_score(self.score)
+        write_score(self.sce)
         if full_exit:
             exit()
         else:
@@ -110,26 +110,26 @@ class Matris(object):
             posY += 1
         p = (posY-1, posX)
         return self.blend(p=p, shadow=True)
-    def fits_in_matrix(self, shape, p):
+    def fits_in_matrix(self, sh, p):
         posY, posX = p
-        for x in range(posX, posX+len(shape)):
-            for y in range(posY, posY+len(shape)):
-                if self.matrix.get((y, x), False) is False and shape[y-posY][x-posX]:
+        for x in range(posX, posX+len(sh)):
+            for y in range(posY, posY+len(sh)):
+                if self.matrix.get((y, x), False) is False and sh[y-posY][x-posX]:
                     return False
         return p
     def request_r(self):
         r = (self.tm_r + 1) % 4
-        shape = self.rotated(r)
+        sh = self.rotated(r)
         y, x = self.tm_p
-        p = (self.fits_in_matrix(shape, (y, x)) or
-                    self.fits_in_matrix(shape, (y, x+1)) or
-                    self.fits_in_matrix(shape, (y, x-1)) or
-                    self.fits_in_matrix(shape, (y, x+2)) or
+        p = (self.fits_in_matrix(sh, (y, x)) or
+                    self.fits_in_matrix(sh, (y, x+1)) or
+                    self.fits_in_matrix(sh, (y, x-1)) or
+                    self.fits_in_matrix(sh, (y, x+2)) or
                     self.fits_in_matrix(shape, (y, x-2)))
         if p and self.blend(shape, p):
             self.tm_r = r
             self.tm_p = p 
-            self.needs_redraw = True
+            self.n_r = True
             return self.tm_r
         else:
             return False  
@@ -137,18 +137,18 @@ class Matris(object):
         posY, posX = self.tm_p
         if direction == 'left' and self.blend(p=(posY, posX-1)):
             self.tm_p = (posY, posX-1)
-            self.needs_redraw = True
+            self.n_r = True
             return self.tm_p
         elif direction == 'right' and self.blend(p=(posY, posX+1)):
             self.tm_p = (posY, posX+1)
-            self.needs_redraw = True
+            self.n_r = True
             return self.tm_p
         elif direction == 'up' and self.blend(p=(posY-1, posX)):
-            self.needs_redraw = True
+            self.n_r = True
             self.tm_p = (posY-1, posX)
             return self.tm_p
         elif direction == 'down' and self.blend(p=(posY+1, posX)):
-            self.needs_redraw = True
+            self.n_r = True
             self.tm_p = (posY+1, posX)
             return self.tm_p
         else:
@@ -182,41 +182,41 @@ class Matris(object):
         return border
     def lock_tm(self):
         self.matrix = self.blend()
-        lines_cleared = self.remove_lines()
-        self.lines += lines_cleared
-        if lines_cleared:
-            if lines_cleared >= 4:
-                self.linescleared_sound.play()
-            self.score += 100 * (lines_cleared**2) * self.combo
-            if not self.played_hsbeaten_sound and self.score > self.hs:
+        ls_cleared = self.remove_ls()
+        self.ls += ls_cleared
+        if ls_cleared:
+            if ls_cleared >= 4:
+                self.lcs.play()
+            self.sce += 100 * (ls_cleared**2) * self.combo
+            if not self.played_hbs and self.sce > self.hs:
                 if self.hs != 0:
-                    self.hsbeaten_sound.play()
-                self.played_hsbeaten_sound = True
-        if self.lines >= self.level*10:
-            self.levelup_sound.play()
-            self.level += 1
-        self.combo = self.combo + 1 if lines_cleared else 1
+                    self.hbs.play()
+                self.played_hbs = True
+        if self.ls >= self.lvl*10:
+            self.lus.play()
+            self.lvl += 1
+        self.combo = self.combo + 1 if ls_cleared else 1
         self.set_tmes()
         if not self.blend():
-            self.gameover_sound.play()
+            self.gos.play()
             self.gameover()   
-        self.needs_redraw = True
-    def remove_lines(self):
-        lines = []
+        self.n_r = True
+    def remove_ls(self):
+        ls = []
         for y in range(MH):
-            line = (y, [])
+            l = (y, [])
             for x in range(MATRIX_WIDTH):
                 if self.matrix[(y,x)]:
-                    line[1].append(x)
-            if len(line[1]) == MATRIX_WIDTH:
-                lines.append(y)
-        for line in sorted(lines):
+                    l[1].append(x)
+            if len(l[1]) == MATRIX_WIDTH:
+                ls.append(y)
+        for l in sorted(ls):
             for x in range(MATRIX_WIDTH):
-                self.matrix[(line,x)] = None
-            for y in range(0, line+1)[::-1]:
+                self.matrix[(l,x)] = None
+            for y in range(0, l+1)[::-1]:
                 for x in range(MATRIX_WIDTH):
                     self.matrix[(y,x)] = self.matrix.get((y-1,x), None)
-        return len(lines)
+        return len(ls)
     def blend(self, shape=None, p=None, matrix=None, shadow=False):
         if shape is None:
             shape = self.rotated()
@@ -233,12 +233,12 @@ class Matris(object):
                 elif shape[y-posY][x-posX]:
                     copy[(y,x)] = ('shadow', self.shadow_block) if shadow else ('block', self.tm_block)
         return copy
-    def construct_surface_of_next_tm(self):
-        shape = self.next_tm.shape
-        surf = Surface((len(shape)*BS, len(shape)*BS), pygame.SRCALPHA, 32)
-        for y in range(len(shape)):
-            for x in range(len(shape)):
-                if shape[y][x]:
+    def construct_sf_of_next_tm(self):
+        sh = self.next_tm.sh 
+        surf = Surface((len(sh)*BS, len(sh)*BS), pygame.SRCALPHA, 32)
+        for y in range(len(sh)):
+            for x in range(len(sh)):
+                if sh[y][x]:
                     surf.blit(self.block(self.next_tm.color), (x*BS, y*BS))
         return surf
 class Game(object):
@@ -253,52 +253,52 @@ class Game(object):
         while True:
             try:
                 timepassed = clock.tick(50)
-                if self.matris.update((timepassed / 1000.) if not self.matris.paused else 0):
+                if self.matris.update((timepassed / 1000.) if not self.matris.p else 0):
                     self.redraw()
             except GameOver:
                 return
     def redraw(self):
-        if not self.matris.paused:
-            self.blit_next_tm(self.matris.surface_of_next_tm)
+        if not self.matris.p:
+            self.blit_next_tm(self.matris.sf_of_next_tm)
             self.blit_info()
-            self.matris.draw_surface()
+            self.matris.draw_sf()
         pygame.display.flip()
     def blit_info(self):
         textcolor = (255, 255, 255)
-        font = pygame.font.Font(None, 30)
+        f = pygame.font.Font(None, 30) 
         width = (WIDTH-(MATRIS_OFFSET+BS*MATRIX_WIDTH+BW*2)) - MATRIS_OFFSET*2
         def renderpair(text, val):
-            text = font.render(text, True, textcolor)
-            val = font.render(str(val), True, textcolor)
+            text = f.render(text, True, textcolor)
+            val = f.render(str(val), True, textcolor)
             surf = Surface((width, text.get_rect().height + BW*2), pygame.SRCALPHA, 32)
             surf.blit(text, text.get_rect(top=BW+10, left=BW+10))
             surf.blit(val, val.get_rect(top=BW+10, right=width-(BW+10)))
             return surf
-        scoresurf = renderpair("Score", self.matris.score)
-        levelsurf = renderpair("Level", self.matris.level)
-        linessurf = renderpair("Lines", self.matris.lines)
+        scesurf = renderpair("Score", self.matris.sce)
+        lvlsurf = renderpair("LVL", self.matris.lvl)
+        lssurf = renderpair("Lines", self.matris.ls)
         combosurf = renderpair("Combo", "x{}".format(self.matris.combo))
-        height = 20 + (levelsurf.get_rect().height + 
-                       scoresurf.get_rect().height +
-                       linessurf.get_rect().height + 
+        height = 20 + (lvlsurf.get_rect().height + 
+                       scesurf.get_rect().height +
+                       lssurf.get_rect().height + 
                        combosurf.get_rect().height )
-        area = Surface((width, height))
-        area.fill(BRC)
-        area.fill(BC, Rect(BW, BW, width-BW*2, height-BW*2))
-        area.blit(levelsurf, (0,0))
-        area.blit(scoresurf, (0, levelsurf.get_rect().height))
-        area.blit(linessurf, (0, levelsurf.get_rect().height + scoresurf.get_rect().height))
-        area.blit(combosurf, (0, levelsurf.get_rect().height + scoresurf.get_rect().height + linessurf.get_rect().height))
-        screen.blit(area, area.get_rect(bottom=HEIGHT-MATRIS_OFFSET, centerx=TRICKY_CENTERX))
+        a = Surface((width, height)) 
+        a.fill(BRC)
+        a.fill(BC, Rect(BW, BW, width-BW*2, height-BW*2))
+        a.blit(lvlsurf, (0,0))
+        a.blit(scesurf, (0, lvlsurf.get_rect().height))
+        a.blit(lssurf, (0, lvlsurf.get_rect().height + scesurf.get_rect().height))
+        a.blit(combosurf, (0, lvlsurf.get_rect().height + scesurf.get_rect().height + lssurf.get_rect().height))
+        screen.blit(a, a.get_rect(bottom=HEIGHT-MATRIS_OFFSET, centerx=TRICKY_CENTERX))
     def blit_next_tm(self, tm_surf):
-        area = Surface((BS*5, BS*5))
-        area.fill(BRC)
-        area.fill(BC, Rect(BW, BW, BS*5-BW*2, BS*5-BW*2))
-        areasize = area.get_size()[0]
+        a = Surface((BS*5, BS*5))
+        a.fill(BRC)
+        a.fill(BC, Rect(BW, BW, BS*5-BW*2, BS*5-BW*2))
+        asize = a.get_size()[0]
         tm_surf_size = tm_surf.get_size()[0]
-        center = areasize/2 - tm_surf_size/2
-        area.blit(tm_surf, (center, center))
-        screen.blit(area, area.get_rect(top=MATRIS_OFFSET, centerx=TRICKY_CENTERX))
+        center = asize/2 - tm_surf_size/2
+        a.blit(tm_surf, (center, center))
+        screen.blit(a, a.get_rect(top=MATRIS_OFFSET, centerx=TRICKY_CENTERX))
 class Menu(object):
     running = True
     def main(self, screen):
@@ -326,10 +326,10 @@ class Menu(object):
             menu.draw(screen)
             pygame.display.flip()
     def construct_hssurf(self):
-        font = pygame.font.Font(None, 50)
+        f = pygame.font.Font(None, 50)
         hs = load_score()
         text = "hs: {}".format(hs)
-        return font.render(text, True, (255,255,255))
+        return f.render(text, True, (255,255,255))
 def construct_nightmare(size):
     surf = Surface(size)
     boxsize,bordersize = 8,1
